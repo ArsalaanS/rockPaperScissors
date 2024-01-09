42cdcd2cb7bfde81fb64e206b5841453ddb3c60e
Arsalaan Siddiqui <arsalaans2003@gmail.com>
2024-01-08 22:45:25 -0500

    made the whole thing. forgot to make commits. will do better next time.

--- script.js
+++ script.js
@@ -11,10 +11,118 @@ account for ties - replay round
 */
 
 const choice = ["Rock", "Paper", "Scissors"]
+let playerChoice;
+let computerChoice;
 
 function getComputerChoice(){
-    let computerChoice = choice[Math.floor(Math.random()* choice.length)];
+    computerChoice = choice[Math.floor(Math.random()* choice.length)];
     return computerChoice;
 };
 
-// console.log(getComputerChoice());
+computerChoice = getComputerChoice();
+
+function getPlayerChoice(){
+    playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+    return playerChoice;
+}
+
+playerChoice = getPlayerChoice();
+
+const winMessage = "You Win! " + playerChoice + " beats " + computerChoice;
+const loseMessage = "You Lose. " + computerChoice + " beats " + playerChoice;
+const drawMessage = "It's a Draw!"
+let result = '';
+
+let computerScore = 0;
+let playerScore = 0;
+
+function playRound(playerChoice, computerChoice){
+
+    getComputerChoice();
+    getPlayerChoice();
+    console.log(playerChoice);
+    console.log(computerChoice);
+    
+    if (playerChoice == computerChoice){
+        result = drawMessage;
+        return result;
+    } else if (playerChoice == "Rock"){
+            if (computerChoice == "Scissors"){
+                result = winMessage;
+                playerScore++;
+                return result;
+                
+            } else if(computerChoice == "Paper"){
+                result = loseMessage;
+                computerScore++;
+                return result;
+            }
+        } else if (playerChoice == "Paper"){
+            if (computerChoice == "Scissors"){
+                result = winMessage;
+                playerScore++;
+                return result;
+            } else if(computerChoice == "Rock"){
+                result = loseMessage;
+                computerScore++;
+                return result;
+            }
+        } else if(playerChoice == "Scissors"){
+            if (computerChoice == "Paper"){
+                result = winMessage;
+                playerScore++;
+                return result;
+            } else if (computerChoice == "Rock"){
+                result = loseMessage;
+                computerScore++;
+                return result;
+            }
+        } 
+
+}
+
+function checkScore(){
+    if (playerScore || computerScore == 3){
+
+    }
+};
+
+function game(){
+
+    // for (var i = 0; i < 5; i++){
+    //     playRound();
+    //     console.log(playRound(playerChoice, computerChoice));
+    //     console.log("Player Score: "+ playerScore);
+    //     console.log("Computer Score: "+ computerScore);
+    // };
+    // while (playerScore || computerScore <= 3){
+    //     // playRound(playerChoice, computerChoice);
+    //     console.log(playRound(playerChoice, computerChoice));
+    //     console.log("Player Score: "+ playerScore);
+    //     console.log("Computer Score: "+ computerScore);
+    // };
+
+    // if (playerScore > computerChoice){
+    //     console.log("You WIN!")
+    // } else {
+    //     console.log("You LOSE!")
+    // };
+
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(playRound(playerChoice, computerChoice));
+
+    console.log("Player: "+ playerScore);
+    console.log("Computer: "+ computerScore);
+
+    if (playerScore > computerScore){
+        console.log("YOU WIN!")
+    } else {
+        console.log("You Lose. Better luck next time!")
+    }
+}
+
+console.log(game());
